d6545455569a5d7da6fc6fc6e094ec0e2d6a6916
Matt Bajorek <bajorek.matthew@gmail.com>
2016-10-05 10:54:53 -0400

    Writes a properties file with three properties.

--- JdbcTest/src/com/example/jdbc/DatabaseTest.java
+++ JdbcTest/src/com/example/jdbc/DatabaseTest.java
@@ -1,10 +1,36 @@
 package com.example.jdbc;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Properties;
+
 public class DatabaseTest {
+	private String dbUrl;
+	private String dbUser;
+	private String dbPassword;
+	
+	
+	public void writeProperties() throws IOException {
+		Properties prop = new Properties();
+		prop.setProperty("dbUrl", "localhost");
+		prop.setProperty("dbUser", "username");
+		prop.setProperty("dbPassword", "password");
+
+		try (OutputStream out = new FileOutputStream("project.properties")) { // file name project.properties
+			prop.store(out, "Database Properties File");
+		}
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		DatabaseTest app = new DatabaseTest();
+		try {
+			app.writeProperties();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
